5ebbd5bd5c7fb18b8074fbf4c76a62817e10e01d
waisdav <waid00@vse.cz>
2026-04-17 11:27:15 +0200

    allignment repaired

--- app/api/docs/alignment/route.js
+++ app/api/docs/alignment/route.js
@@ -1,13 +1,13 @@
 import OpenAI from "openai";
 import { createDocsAlignmentHandler } from "../../../../lib/docs-alignment.js";
-import { resolveDocumentById } from "../../../../lib/document-store.js";
 
 export const runtime = "nodejs";
 
 const rateLimitMap = new Map();
 
-export const POST = createDocsAlignmentHandler({
+const handler = createDocsAlignmentHandler({
   rateLimitMap,
-  resolveDocumentById,
   openaiClient: new OpenAI({ apiKey: process.env.OPENAI_API_KEY ?? "" }),
 });
+
+export const POST = handler;
